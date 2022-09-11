da5665812d6cca9ec11b9072e0841bcc8dbee405
Ari Effendi <ari@arped.id>
2022-09-12 00:58:26 +0800

    Remove static

--- app/Services/ComposerService.php
+++ app/Services/ComposerService.php
@@ -9,40 +9,40 @@ use Symfony\Component\Process\Process;
  */
 class ComposerService
 {
-    public static $command = [
+    private $command = [
         'composer',
     ];
 
-    public static function createProject($package, $name)
+    public function createProject($package, $name)
     {
-        array_push(self::$command, 'create-project', $package, $name);
+        array_push($this->command, 'create-project', $package, $name);
 
-        self::run();
+        $this->run();
     }
 
-    public static function createLaravelProject($name)
+    public function createLaravelProject($name)
     {
         self::createProject('laravel/laravel', $name);
     }
 
-    public static function require($package, $dev = false, $global = false)
+    public function require($package, $dev = false, $global = false)
     {
-        array_push(self::$command, 'require', $package);
+        array_push($this->command, 'require', $package);
 
         if ($dev) {
-            array_push(self::$command, '--dev');
+            array_push($this->command, '--dev');
         }
 
         if ($global) {
-            array_splice(self::$command, 1, 1, ['global', 'require']);
+            array_splice($this->command, 1, 1, ['global', 'require']);
         }
 
-        self::run();
+        $this->run();
     }
 
-    public static function run()
+    public function run()
     {
-        $process = new Process(self::$command);
+        $process = new Process($this->command);
         $process->run();
 
         if (! $process->isSuccessful()) {
